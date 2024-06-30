4e7b56cc19cc568289b0e8f9ff033b4d5263b0b4
Mohammed Saed <mohd.a.saed@gmail.com>
2024-06-30 08:47:14 +0300

    Implement auth screens

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+function AuthLayout() {
+	return <Stack />;
+}
+
+export default AuthLayout;
---
+++ src/app/(auth)/signin.tsx
@@ -0,0 +1,65 @@
+import Button from '@/components/Button';
+import Colors from '@/constants/Colors';
+import { Link, Stack } from 'expo-router';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+function SignIn() {
+	const [email, setEmail] = useState<string>();
+	const [password, setPassword] = useState<string>();
+
+	return (
+		<View style={styles.container}>
+			<Stack.Screen options={{ title: 'Sign in' }} />
+
+			<Text style={styles.label}>Email</Text>
+			<TextInput
+				style={styles.input}
+				placeholder="jon@gmail.com"
+				value={email}
+				onChangeText={setEmail}
+			/>
+
+			<Text style={styles.label}>Password</Text>
+			<TextInput
+				style={styles.input}
+				secureTextEntry
+				value={password}
+				onChangeText={setPassword}
+			/>
+
+			<Button text="Sign in" />
+			<Link href="/signup" style={styles.textButton}>
+				Create an account
+			</Link>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		padding: 20,
+	},
+	label: {
+		color: 'gray',
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: 'gray',
+		padding: 10,
+		marginTop: 5,
+		marginBottom: 20,
+		backgroundColor: 'white',
+		borderRadius: 5,
+	},
+	textButton: {
+		alignSelf: 'center',
+		fontWeight: 'bold',
+		color: Colors.light.tint,
+		marginVertical: 10,
+	},
+});
+
+export default SignIn;
